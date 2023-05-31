c4583d5c91647fccb5ea047eb00afbf277359f61
vvomes <gomeswilliandes@hotmail.com>
2023-05-30 21:14:22 -0300

    script contato

---
+++ src/js/scripts.js
@@ -0,0 +1,13 @@
+function contatar() {
+	if (document.getElementById('name').value == '')
+		return alert('Por favor, insira um nome')
+
+	if (document.getElementById('email').value == '' && document.getElementById('phoneNumber').value == '')
+		return alert('Por favor, insira um e-mail ou número de telefone')
+
+	if (document.getElementById('message').value == '')
+		return alert('Por favor insira uma mensagem')
+
+	alert('Mensagem enviada com sucesso. Obrgiado por entrar em contato conosco!')
+
+}
