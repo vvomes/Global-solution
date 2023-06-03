5a5a676f41c733a20f7f7580cc7e1d3f2c543abe
Jeferson Noronha <jeferson_noronha@yahoo.com.br>
2023-06-03 07:34:37 -0300

    Create Footer.jsx

---
+++ src/components/Index/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <p>© 2023 Nome da Empresa. Todos os direitos reservados.</p>
+        <p>Endereço: 123 Rua do Exemplo, Cidade, Estado</p>
+        <p>Telefone: (00) 1234-5678</p>
+        <p>Email: exemplo@empresa.com</p>
+      </FooterContent>
+    </FooterContainer>
+  );
+}
+
+const FooterContainer = styled.footer`
+  background-color: #1c3048; /* Cor de fundo */
+  color: #fff; /* Cor do texto */
+  padding: 20px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+`;
+
+const FooterContent = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 14px;
+`;
