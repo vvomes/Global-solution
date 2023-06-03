6bd9644e75acfc5d4277ec931f861a45848d67dd
Jeferson Noronha <jeferson_noronha@yahoo.com.br>
2023-06-03 07:34:58 -0300

    Create Grafico.jsx

---
+++ src/pages/Grafico.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Index/Header"
+import Footer from "../components/Index/Footer"
+
+
+export default function Grafico() {
+    return (
+
+        <>
+            <Header />
+            <h1>Ola você esta na pagina Grafico</h1>
+            <Footer/>
+        </>
+    
+
+    )
+}
