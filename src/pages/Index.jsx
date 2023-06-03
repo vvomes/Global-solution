9cf05aad04e11971193f6242ceea3f134eae7d4a
Jeferson Noronha <jeferson_noronha@yahoo.com.br>
2023-06-03 07:34:54 -0300

    Create Index.jsx

---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Index/Hero";
+import Header from "../components/Index/Header";
+import Hero from "../components/Index/Footer";
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <Hero />
+            <Footer />
+           
+        </>
+    )
+}
