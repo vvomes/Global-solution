e54aea6ce0feae2ae9e3094e2549f262ffe96224
Jeferson Noronha <jeferson_noronha@yahoo.com.br>
2023-06-03 07:35:03 -0300

    Create FaleConosco.jsx

---
+++ src/pages/FaleConosco.jsx
@@ -0,0 +1,18 @@
+import Header from "../components/Index/Header"
+import Footer from "../components/Index/Footer"
+
+
+export default function FaleConosco() {
+    return (
+
+        <>
+            <Header />
+            
+            <h1>Ola você esta na pagina Fale Conosco</h1>
+
+            <Footer/>
+        </>
+
+
+    )
+}
