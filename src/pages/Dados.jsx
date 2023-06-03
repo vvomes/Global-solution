b0242a71a742a5f8e39bd639c30a9abddaae347f
Jeferson Noronha <jeferson_noronha@yahoo.com.br>
2023-06-03 07:35:06 -0300

    Create Dados.jsx

---
+++ src/pages/Dados.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Index/Header"
+import Footer from "../components/Index/Footer"
+
+
+export default function Dados() {
+    return (
+
+        <>
+            <Header />
+            <h1>Ola você esta na pagina Dados</h1>
+            <Footer/>
+        </>
+
+
+    )
+}
